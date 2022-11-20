4159862da048f2e74f12225e4c5d15cbf26bbbac
manuel londono <mLondono@q10.com>
2022-11-20 14:52:38 -0500

    ci-cd: change front to railway

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,10 +4,6 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    // @ts-ignore
-    port: process.env.PORT || 3001
-  },
   plugins: [react()],
   test: { environment: 'happy-dom' },
   base: './',
