e1d6bdb9c743a1a454c550e1c817754633722702
manuel londono <mLondono@q10.com>
2022-11-19 20:41:28 -0500

    doc: add documentation endpoints

---
+++ back/CustomSettings.ts
@@ -0,0 +1,6 @@
+export const CustomSettings = {
+  supabase: {
+    projectUrl: process.env.PROJECT_URL,
+    apiKey: process.env.API_KEY,
+  },
+};
--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,15 @@
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'documentation'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
